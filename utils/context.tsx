8d85d2513370947d2125daaf699e2535fcbe9e5d
Frank Ezeh <ezehfrank87@gmail.com>
2023-03-09 17:08:43 +0100

    fix issue

--- utils/context.tsx
+++ utils/context.tsx
@@ -37,8 +37,7 @@ export function OrderBookProvider({ children }: { children: ReactNode }) {
   const [orderType, setOrderType] = useState<Order>("asks");
   const [payToken, setPayToken] = useState<Token | undefined>();
   const [receiveToken, setReceiveToken] = useState<Token | undefined>();
-  const ORDER_LENGTH = orderType === "asks" ? 16 : 8;
-  //console.log({ data });
+
   const getCurrentState = async () => {
     const url =
       "https://api.0x.org/orderbook/v1?quoteToken=0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2&baseToken=0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
@@ -65,26 +64,28 @@ export function OrderBookProvider({ children }: { children: ReactNode }) {
     getCurrentState();
   }, []);
 
-  const priceExists = (price: number) => {
-    return data[orderType].some((data) => data.price === price);
+  const ORDER_LENGTH = state?.[orderType]?.records.length;
+
+  const priceExists = (price: string) => {
+    return data[orderType].some((data) => data.price.toFixed(2) === price);
   };
 
-  const updateOrder = (price: number) => {
-    const newOrderData = data[orderType].find((data) => data.price === price);
+  const updateOrder = (price: string, newData: Data) => {
     const newOrderDataIndex = data[orderType].findIndex(
-      (data) => data.price === price
+      (data) => data.price.toFixed(2) === price
     );
     const updatedData = { ...data };
 
-    updatedData[orderType][newOrderDataIndex] = newOrderData as Data;
-  
+    updatedData[orderType][newOrderDataIndex] = newData as Data;
     if (newOrderDataIndex !== -1) {
-    setData(updatedData);
+      setData(updatedData);
     }
   };
 
-  const removeOrder = (price: number, data: ParsedData) => {
-    const newOrderData = data[orderType].filter((data) => data.price !== price);
+  const removeOrder = (price: string, data: ParsedData) => {
+    const newOrderData = data[orderType].filter(
+      (data) => data.price.toFixed(2) !== price
+    );
 
     setData({
       ...data,
@@ -93,7 +94,7 @@ export function OrderBookProvider({ children }: { children: ReactNode }) {
   };
 
   const addOrder = (newData: Data[]) => {
-    setData({ ...data, [orderType]: { ...data[orderType], ...newData } });
+    setData({ ...data, [orderType]: [...data[orderType], ...newData] });
   };
 
   const getUpdate = (payload: Record[]) => {
@@ -102,25 +103,24 @@ export function OrderBookProvider({ children }: { children: ReactNode }) {
 
     if (makerAmount > takerAmount) setOrderType("bids");
 
-    let orderLength = state![orderType]?.records.length;
+    let orderLength = data![orderType].length;
 
     let socketData =
       orderType === "asks" ? addAskData(payload) : addBidData(payload);
-      
-      console.log("price", socketData[0].price);
+
+    let price = socketData[0].price.toFixed(2);
+    console.log("price", price);
     
-      // // If new quantity is zero - delete the order for that price level
-    if (socketData[0].quantity === 0 && orderLength > ORDER_LENGTH) {
-      removeOrder(socketData[0].price, data);
+    // // If new quantity is zero - delete the order for that price level
+    if (socketData[0].quantity == 0 && orderLength > ORDER_LENGTH!) {
+      removeOrder(price, data);
     } else {
       // If the price level exists and the quantity is not zero, update the order book
-      if (priceExists(socketData[0].price)) {
-        console.log("data", "Price dey oh");
-
-        updateOrder(socketData[0].price);
+      if (priceExists(price)) {
+        updateOrder(price, socketData[0]);
       } else {
         //If the price level doesn't exist in the orderbook and there are less than order length, add it
-        if (orderLength < ORDER_LENGTH) {
+        if (orderLength < ORDER_LENGTH!) {
           addOrder(socketData);
         }
       }
